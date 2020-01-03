47b5c75e4acb22f83fa83d8bf5d5c249a2115fde
郝程宇 <haochengyu@kuaifawu.com>
2020-01-03 16:58:48 +0800

    迁移网络/路由表

---
+++ views/route-table/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <page-header title="路由表" />
+    <page-body>
+      <route-list :id="listId" />
+    </page-body>
+  </div>
+</template>
+
+<script>
+import RouteList from './components/List'
+
+export default {
+  name: 'NetworkIndex',
+  components: {
+    RouteList,
+  },
+  data () {
+    return {
+      listId: 'RouteList',
+    }
+  },
+}
+</script>
